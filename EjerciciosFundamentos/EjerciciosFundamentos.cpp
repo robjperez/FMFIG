8a3ec09ddea885770e28478d7860cde6a7f2a409
Roberto Perez <robj.perez@gmail.com>
2020-10-18 16:39:25 +0200

    Ex1 and 2. Add sample plane

--- EjerciciosFundamentos/EjerciciosFundamentos.cpp
+++ EjerciciosFundamentos/EjerciciosFundamentos.cpp
@@ -9,17 +9,46 @@ int main()
 {
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
-    igl::readPLY("..\\gargoyle.25kv.ply", V, F);
+    //igl::readPLY("..\\gargoyle.25kv.ply", V, F);
+    igl::readPLY("..\\plane.ply", V, F);
     
+    cout << "Vertices = " << endl << V << endl;
+    cout << "Facetas = " << endl << F << endl;
+    cout << endl << endl;
     // Ejercicio 1
-    Eigen::Vector3d centroide = { 0, 0, 0 };
+    /*Eigen::Vector3d centroide = { 0, 0, 0 };
 
     for (size_t i = 0; i < V.rows(); i++) {
         centroide += V.row(i);             
     }
     centroide /= V.rows();
 
-    cout << "Centroide= " << endl << centroide << endl;
+    cout << "Centroide= " << endl << centroide << endl;*/
+
+    // Ejercicio 2
+    // Calcular las normales por faceta
+ /*   std::vector<Eigen::Vector3d> normalesFacetas;
+    normalesFacetas.reserve(F.size());
+    for (size_t i = 0; i < F.rows(); i++) {
+        auto vA = V.row(F.row(i)[0]);
+        auto vB = V.row(F.row(i)[1]);
+        auto vC = V.row(F.row(i)[2]);
+
+        Eigen::Vector3d v1 = vB - vA;
+        Eigen::Vector3d v2 = vC - vA;
+
+        Eigen::Vector3d n = v1.cross(v2);
+        n.normalize();
+        normalesFacetas.push_back(n);
+    }*/
+
+    // Ejercicio 3
+    // Normales por vertice ponderando por area
+    std::vector<Eigen::Vector3d> normalesVertices;
+    normalesVertices.reserve(V.size());
+    for (size_t i = 0; i < V.rows(); i++) {
+        
+    }
 
     // Plot the mesh
     /*igl::opengl::glfw::Viewer viewer;
